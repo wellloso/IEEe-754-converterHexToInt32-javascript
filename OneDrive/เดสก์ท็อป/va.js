1fe2f715a99046c334db8f889a714f7773324041
nattakorn <wellloso@gmail.com>
2024-06-04 21:11:41 +0700

    first commit

---
+++ OneDrive/เดสก์ท็อป/va.js
@@ -0,0 +1,27 @@
+var str = '0x46495FD0';
+console.log(parseFloat(str));
+
+function parseFloat(str) {
+  var float = 0, sign, order, mantiss,exp,
+      int = 0, multi = 1;
+  if (/^0x/.exec(str)) {
+    int = parseInt(str,16);
+  }else{
+    for (var i = str.length -1; i >=0; i -= 1) {
+      if (str.charCodeAt(i)>255) {
+        console.log('Wrong string parametr'); 
+        return false;
+      }
+      int += str.charCodeAt(i) * multi;
+      multi *= 256;
+    }
+  }
+  sign = (int>>>31)?-1:1;
+  exp = (int >>> 23 & 0xff) - 127;
+  mantissa = ((int & 0x7fffff) + 0x800000).toString(2);
+  for (i=0; i<mantissa.length; i+=1){
+    float += parseInt(mantissa[i])? Math.pow(2,exp):0;
+    exp--;
+  }
+  return float*sign;
+}
